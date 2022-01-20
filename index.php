dde00dd8323c9cc431109319370418b98e7f624c
Victor Passos <vicfilipe@gmail.com>
2022-01-19 22:45:42 -0300

    Add tax calc function

--- index.php
+++ index.php
@@ -29,9 +29,23 @@
 
             foreach($products as $key => $value){
                 if($value <= $credit){
-                    echo "<p>".$key."</p>";
+                    echo "<p> ".$key." </p>";
                 }
             }
+
+            $amount=800;
+            $taxRate=0.0825;
+            $addedTax=$amount*$taxRate; // amount = 800, tax = .0825
+            echo $addedTax;
+
+            function tax_calc($amount,$tax){
+              $calculate_tax = $amount*$tax;
+              $amount = round($amount+$calculate_tax,2);
+              return $amount;
+            }
+            echo "<br>";
+
+            echo tax_calc(750,0.223);
         ?>
     </body>
 </html>
