97839762483486b01cff266ab0f4471608e3c854
Victor Passos <vicfilipe@gmail.com>
2022-01-19 21:57:18 -0300

    Initial php setup

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Store</title>
+    </head>
+    <body>
+        <?php
+        // comments
+        echo "<h1>Welcome!</h1>"
+        ?>
+    </body>
+</html>
